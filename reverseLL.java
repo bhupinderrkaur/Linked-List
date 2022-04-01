784b0ff78a5c6e612fd1fc2be30a24799fcb77ce
Bhupinder Kaur <99190152+bhupinderrkaur@users.noreply.github.com>
2022-04-01 14:23:19 +0530

    Update reverseLL.java

--- reverseLL.java
+++ reverseLL.java
@@ -1,6 +1,6 @@
 class LinkedList {
 
- 
+ //iterative
     Node reverse(Node node)
     {
         Node prev = null;
@@ -14,5 +14,20 @@ class LinkedList {
         }
         node = prev;
         return node;
+    }
+ 
+  //recursive
+ static Node reverse(Node head)
+    {
+        if (head == null || head.next == null)
+            return head;
+ 
+       
+        Node rest = reverse(head.next);
+        head.next.next = head;
+ 
+        head.next = null;
+ 
+        return rest;
     }
     }
